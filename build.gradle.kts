4aee359a1563446bca55aca800b5e09bab4d2bfd
Sander Ruul <sander.ruul@gmail.com>
2021-07-12 23:17:54 +0300

    chore: testing artifact upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,4 +26,15 @@ dependencies {
 
 publishing {
 
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = "https://maven.pkg.github.com/octocat/hello-world"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
 }
