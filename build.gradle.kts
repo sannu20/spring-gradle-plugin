e34ec1f62bff052550fc2426aa37eb51d8ab334d
Sander Ruul <sander.ruul@gmail.com>
2021-11-19 23:16:59 +0200

    upgrade spring to 2.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.2")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3")
 }
